3ce0dd0b1d3af3d25e53d6e5bfd747f61fa6d2a8
FrostLeaf15 <85655364+FrostLeaf15@users.noreply.github.com>
2025-12-05 15:16:30 +0700

    Update: Galeri Page

--- database/migrations/2025_02_13_065818_create_galeris_table.php
+++ database/migrations/2025_02_13_065818_create_galeris_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('gambar');
             $table->text('deskripsi');
+            $table->text('link');
             $table->timestamps();
         });
     }
